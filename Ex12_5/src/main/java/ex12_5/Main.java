3341ab89731aff1d337e5ff5f3b724afcb244c13
dreddFirst <treba2005@mail.ru>
2020-01-05 10:32:31 +0200

    update ex12_5

--- Ex12_5/src/main/java/ex12_5/Main.java
+++ Ex12_5/src/main/java/ex12_5/Main.java
@@ -11,10 +11,27 @@ package ex12_5;
  */
 public class Main {
     public static void main(String[] args){
+        StringBuilder str = new StringBuilder("welllcommmmeee");
+        System.out.println(repeatChar(str));
+        
         System.out.println("12.5.2 = "+strReplace("it is a string"));
         
         String sb = "ab5c2d4ef12s";
         System.out.println("12.5.3 = "+sumNumbers(sb));
+        
+    }
+    
+    static int repeatChar(StringBuilder str){
+        int n = 0;
+        int count = 0;
+        for(int i = 0; i < str.length()-2; i++){
+            if(str.charAt(i) == str.charAt(i+1)){
+                if(str.charAt(i+1)==str.charAt(i+2)){
+                    n++;
+                }
+            }
+        }
+        return n;
     }
     
     static String strReplace(String str){
